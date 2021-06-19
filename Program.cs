2f633438087e0f156f57b7e747b604818310d807
Mateus Nadal <nadalmateus@hotmail.com>
2021-06-19 16:52:30 -0300

    Meotodos adicionais

--- Program.cs
+++ Program.cs
@@ -8,7 +8,14 @@ namespace csharpStrings
         {
             var text = "Esse texto é um teste";
             Console.WriteLine(text.IndexOf("é"));
-            Console.WriteLine(text.LastIndexOf("s"));
+            Console.WriteLine(text.ToLower());
+            Console.WriteLine(text.ToUpper());
+            Console.WriteLine(text.Insert(11, "aqui "));
+            Console.WriteLine(text.Remove(5, 5));
+            Console.WriteLine(text.Length);
+
+
+
 
         }
     }
